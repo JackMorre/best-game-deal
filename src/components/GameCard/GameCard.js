481f687c075a9a91d46521bcc91f13cb37c20b8f
Jack <jack.morrell79@googlemail.com>
2023-11-15 08:35:18 +0000

    basic render of list with static data

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -2,19 +2,19 @@ import BuldursGateImg from "./BuldursGateImg.webp";
 
 import "./gameCard.css";
 
-export const GameCard = () => {
+export const GameCard = ({ title, image, percentage, retail, price }) => {
   return (
     <li className="card-c">
       <div className="img-c">
-        <img alt="img of the game Buldurs gate III" src={BuldursGateImg}></img>
+        <img alt="img of the game Buldurs gate III" src={image}></img>
       </div>
       <div className="text-c">
-        <h3>Game title</h3>
+        <h3>{title}</h3>
         <div className="data-c">
-          <p className="percentage">-80%</p>
+          <p className="percentage">-{Math.floor(+percentage)}%</p>
           <div className="price">
-            <p className="original-price">£69.99</p>
-            <p className="best-price">£49.99</p>
+            <p className="original-price">£{retail}</p>
+            <p className="best-price">£{price}</p>
           </div>
         </div>
       </div>
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import { dataForSearch } from "../local-json/searchData";
 
 export const Search = () => {
-  const [url, setUrl] = useState();
+  const [url, setUrl] = useState("wow");
   // declare a state variable selectedOrg and set the default value as undefined
   const [data, setData] = useState();
 
@@ -13,19 +13,32 @@ export const Search = () => {
     () => {
       // check if selectedOrg is truthy
       if (url) {
-        console.log(dataForSearch);
         // declare a async function to fetch data from an API using the Web API of fetch
-        const fetchData = async () => {
-          // use axios get method to make a GET request to an URL for the value of the selectedOrg
-          const { data } = await axios.get("./gameInfo.json");
 
-          // the setter function from useState for repos is used to change the state of repos to the data from the API
-          console.log(data);
-          // setData(data);
-        };
+        const newArray = Promise.all(
+          dataForSearch.map(async (game) => {
+            const fetchData = async () => {
+              // use axios get method to make a GET request to an URL for the value of the selectedOrg
+              const { data } = await axios.get("./gameInfo.json");
 
-        // execute the fetchData function
-        fetchData();
+              const gameUpdated = { ...game, bestDeal: data.deals[0] };
+
+              return gameUpdated;
+
+              // the setter function from useState for repos is used to change the state of repos to the data from the API
+
+              // setData(data);
+            };
+
+            // execute the fetchData function
+            const data = await fetchData();
+            return await data;
+          })
+        ).then((results) => {
+          console.log(results);
+          setData(results);
+        });
+        // console.log(newArray);
       }
     },
     // add selectedOrg to the list of dependencies which will execute the callback function every time something in the list changes
@@ -33,10 +46,27 @@ export const Search = () => {
   );
   return (
     <div style={{ padding: "1rem" }}>
-      <h2 style={{ color: "black" }}>Search</h2>
-      <ul style={{ listStyle: "none", padding: "0" }}>
-        <GameCard />
-      </ul>
+      {!data ? (
+        <h2 style={{ color: "black" }}>no Searches</h2>
+      ) : (
+        <div>
+          <h2 style={{ color: "black" }}>Search</h2>
+          <ul style={{ listStyle: "none", padding: "0" }}>
+            {data.map((game) => {
+              return (
+                <GameCard
+                  key={game.gameID}
+                  title={game.external}
+                  image={game.thumb}
+                  percentage={game.bestDeal.savings}
+                  retail={game.bestDeal.retailPrice}
+                  price={game.bestDeal.price}
+                />
+              );
+            })}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
