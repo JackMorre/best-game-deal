59d257c1b15b113bc4b5dbee1c253b3c0140d7e6
isaaco1 <isaacorzech@gmail.com>
2023-11-20 20:26:48 +0000

    Updated No Match Cards

--- src/components/IntroCard/IntroCard.js
+++ src/components/IntroCard/IntroCard.js
@@ -1,5 +1,6 @@
 import "./IntroCard.css";
 import frontImage from "./GamingPerson.jpeg";
+import stickerImage from "./90-off-sticker.png"
 
 export const IntroCard = () => {
   return ( 
@@ -7,10 +8,14 @@ export const IntroCard = () => {
       <div className="container">
         <img src={frontImage} alt="person gaming on monitor"/>
         <div className="bottom-left">
+          
           <h1 className="deals-h1">DEALS</h1>
           <p className="p-text">for your favourite games.</p><br></br>
           <p className="p-text">All in one place.</p>
         </div>
+          <div className="up-right">
+            <img className="deals-sticker" src={stickerImage} alt="90% off sticker"/>
+          </div>
       </div>
     </>
   )
---
+++ src/components/MainNavBar/MainNavBar.js
@@ -0,0 +1,74 @@
+import { FiMenu } from "react-icons/fi";
+import { IoClose } from "react-icons/io5";
+import "./MainNavBar.css";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const pages = [
+  { title: "Search", url: "/search" },
+  { title: "Watch List", url: "/watchlist" },
+  { title: "Deals", url: "/deals" },
+];
+
+export const MainNavBar = () => {
+  const [mobile, setMobile] = useState(true);
+  const [open, setOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth);
+    }
+
+    if (width >= 700) {
+      setMobile(false);
+    } else {
+      setMobile(true);
+    }
+
+    window.addEventListener("resize", handleResize);
+  }, [width]);
+
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/`;
+    navigate(path);
+  };
+
+  console.log(width);
+  function handleOpen() {
+    setOpen(!open);
+  }
+  return (
+    <header className="header">
+      <p className="company-name" onClick={routeChange}>
+        Acme ❌ Games
+      </p>
+
+      <nav
+        className={`main-nav ${width > 700 ? "desktop" : ""} ${
+          !open && width < 700 ? "open" : ""
+        }`}
+      >
+        <ul className="list-items">
+          {pages.map((page) => {
+            return (
+              <li key={page.title}>
+                <a href={page.url}>{page.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+
+      {mobile ? (
+        <div className="mobile-nav-c" onClick={handleOpen}>
+          {!open ? (
+            <FiMenu className="icon" />
+          ) : (
+            <IoClose className="icon close" />
+          )}
+        </div>
+      ) : undefined}
+    </header>
+  );
+};
