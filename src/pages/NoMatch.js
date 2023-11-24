f3af4b65bada4a5b896dd34a2fe4d7ef4385267c
Jack <jack.morrell79@googlemail.com>
2023-11-24 18:25:33 +0000

    updating error

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -7,7 +7,7 @@ export const NoMatch = () => {
     <div className="noMatch">
       <h1>Oops!</h1>
       <h2 className="error-margin">
-        We couldn't find the game you were looking
+        We couldn't find the game you were looking for!
       </h2>
       <Link to="/" id="linkHome">
         Home
